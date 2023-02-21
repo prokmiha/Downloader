e3f3ca8ce1856e1487c4004f44e5d2660ee33eee
prokmiha <prokmiha@gmail.com>
2023-02-21 21:46:48 +0200

    clearing link entry

--- yt_download.py
+++ yt_download.py
@@ -1,20 +1,24 @@
 import time
-from tkinter import *
+import tkinter as tk
 from pytube import YouTube
+import clipboard
 import os
 
-root = Tk()
+root = tk.Tk()
 root_w, root_h = 640, 480
 root_ws, root_hs = root.winfo_screenwidth(), root.winfo_screenheight()
 root_x, root_y = (root_ws / 2) - (root_w / 2), (root_hs / 2) - (root_h / 2)
 root.geometry('%dx%d+%d+%d' % (root_w, root_h, root_x, root_y))
 root.resizable = False
 root.title("Download your favourite video from YouTube!")
-Label(root, text="YouTube Video Downloader", font="arial 20 bold").place(x=125, y=10)
-link = StringVar()
-Label(root, text="Paste Your Link Here", font="arial 15").place(x=225, y=200)
+label_name = tk.Label(root, text="YouTube Video Downloader", font="arial 20 bold")
+label_name.place(x=125, y=10)
+link = tk.StringVar()
+label_description = tk.Label(root, text="Paste Your Link Here", font="arial 15")
+label_description.place(x=225, y=200)
 
-Entry(root, width=95, textvariable=link).place(x=35, y=230)
+entry = tk.Entry(root, width=95, textvariable=link)
+entry.place(x=35, y=230)
 
 
 def downloader():
@@ -57,17 +61,18 @@ def downloader():
             os.rename('E:\\yt_tests\\final_0.mp4', f'E:\\yt_tests\\final_{i}.mp4')
             break
     ready_window()
+    entry.delete(0, 'end')
 
 
 def ready_window():
-    ready = Tk()
+    ready = tk.Tk()
     ready_w, ready_h = 300, 150
     ready_x, ready_y = (root_ws / 2) - (ready_w / 2), (root_hs / 2) - (ready_h / 2)
     ready.geometry('%dx%d+%d+%d' % (ready_w, ready_h, ready_x, ready_y))
     ready.resizable = False
     ready.attributes('-topmost', True)
-    Label(ready, text="Downloaded", font="arial 20 bold").place(x=62, y=30)
-    Button(ready, text="OK", font="arial 15", bg="violet", padx=2,
+    tk.Label(ready, text="Downloaded", font="arial 20 bold").place(x=62, y=30)
+    tk.Button(ready, text="OK", font="arial 15", bg="violet", padx=2,
            command=ready.destroy, ).place(x=125, y=80)
     root.attributes('-topmost', True)
 
@@ -77,9 +82,9 @@ def exit():
     root.destroy()
 
 
-Button(root, text="EXIT", font="arial 15 bold", bg="grey", fg='white', padx=2,
+tk.Button(root, text="EXIT", font="arial 15 bold", bg="grey", fg='white', padx=2,
        command=exit).place(x=285, y=310)
-Button(root, text="DOWNLOAD", font="arial 15", bg="violet", padx=2,
+tk.Button(root, text="DOWNLOAD", font="arial 15", bg="violet", padx=2,
        command=downloader).place(x=250, y=260)
 
 root.mainloop()
